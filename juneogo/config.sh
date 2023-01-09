1bd8e5ddf798dc51e95dac2311600ba80800d2d4
corentinDallenogare <corentda@hotmail.fr>
2023-01-09 10:52:26 +0100

    update generic name for email and domaine + change comment

--- juneogo/config.sh
+++ juneogo/config.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Vérifie si le fichier .juneogo/staking/staker.crt existe
+# Check if the file .juneogo/staking/staker.crt exists
 if [ -f ".juneogo/staking/staker.crt" ]; then
-  # Vérifie si les lignes "db-dir", "log-dir", "staking-tls-cert-file" et "staking-tls-key-file" existent dans le fichier .juneogo/config.json
+  # Check if the lines "staking-tls-cert-file" and "staking-tls-key-file" exist in the file .juneogo/config.json
   if grep -q '"staking-tls-cert-file"' .juneogo/config.json && grep -q '"staking-tls-key-file"' .juneogo/config.json; then
-    # Ne fait rien
+    # Do nothing
     echo "Lines already present in the file"
     :
   else
-    # Ajoute les lignes manquantes au fichier .juneogo/config.json
+    # Add missing lines to the file .juneogo/config.json
      echo "add line into the file"
      sed -i '1r config.txt' .juneogo/config.json
             sed -i '1a\' .juneogo/config.json
@@ -16,7 +16,7 @@ if [ -f ".juneogo/staking/staker.crt" ]; then
     
   fi
 else
-  # Supprime les lignes "db-dir", "log-dir", "staking-tls-cert-file" et "staking-tls-key-file" du fichier .juneogo/config.json
+  # Remove the lines "staking-tls-cert-file" and "staking-tls-key-file" from the file .juneogo/config.json
   sed -i '/^\s*"staking-tls-cert-file"/d' .juneogo/config.json
   sed -i '/^\s*"staking-tls-key-file"/d' .juneogo/config.json
 fi
